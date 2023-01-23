0d95e0e30609641af9ef86f5eab0744c59c6766e
Plamen Georgiev <p.olegov@yahoo.com>
2023-01-23 11:58:31 +0200

    Solve

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,34 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
 
-  const ul = document.querySelector("ul");
+    const ul = document.querySelector("ul");
+
+    let url = "https://pokeapi.co/api/v2/pokemon?limit=10";
+
+    function checkStatus(responce) {
+        if (responce.status >= 200 && responce.status < 300) {
+            return Promise.resolve(responce);
+        } else {
+            return Promise.reject(new Error(responce.statusText));
+        }
+    }
+
+    function toJSON(responce) {
+        return responce.json();
+    }
+
+
+    fetch(url)
+        .then(checkStatus)
+        .then(toJSON)
+        .then((data) => {
+            data.results.forEach(d => {
+                let li = document.createElement("li");
+                li.innerText = d.name;
+                ul.append(li);
+            });
+        });
+
 });
